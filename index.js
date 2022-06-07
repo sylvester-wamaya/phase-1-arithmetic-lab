7d325e704a811ea05630f06a5b0d8bc93d8b6a28
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2022-06-08 00:21:08 +0300

    commit tree ffd8202268090e150dbb1c911655e1b0fc73345c

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 =31; 
+const num2 =2;
+const num3 = 7;
+const num4 = 32;
+let multiply = num1 * num2; 
+let random = Math.floor(Math.random() * 10) + 1;
+const mod = num4 % num3;
+const max = Math.max(1,3,20,15);
